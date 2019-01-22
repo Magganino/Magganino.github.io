60d4613e8fa6bb3aa146aad2e871df7f7c646126
Magnus Nordin <nordin.magnus@outlook.com>
2019-01-22 15:16:41 +0100

    fixed build

---
+++ precache-manifest.2d4117842c44e2444f81ac29242b88c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4907c42dc0f8ebce39a",
+    "url": "/static/css/main.ea0d3ffd.chunk.css"
+  },
+  {
+    "revision": "f4907c42dc0f8ebce39a",
+    "url": "/static/js/main.f4907c42.chunk.js"
+  },
+  {
+    "revision": "8326fffb74d2f465f070",
+    "url": "/static/js/1.8326fffb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f96cd1711fd0e3348ebb5a52e3835640",
+    "url": "/index.html"
+  }
+];
